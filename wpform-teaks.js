65304cf63a2fa65c755cfc62229b312e3a8bfa65
Cylas Kiganda <kigandacylas@gmail.com>
2022-03-29 10:37:01 +0300

    commit-3

--- wpform-teaks.js
+++ wpform-teaks.js
@@ -3,8 +3,9 @@ jQuery(document).ready(function ( $ ){
     $( document ).ajaxComplete(function() {
         console.log("preview active");
         $(".wpforms-entry-preview.wpforms-entry-preview-compact .wpforms-entry-preview-label").each(function(){
-            if(!$this.find("#required_adttn")){
-                $this.append('<span class="wpforms-required-label">必須</span>');
+            $(this).append('<span id="required_adttn" class="wpforms-required-label">必須</span>');
+            if(!$(this).find("#required_adttn")){
+                $(this).append('<span id="required_adttn" class="wpforms-required-label">必須</span>');
             }
         });
       });
